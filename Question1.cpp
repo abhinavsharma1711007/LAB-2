e687d557b3ada0927332a32335b848cb644726f5
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:42:53 +0530

    Create Question1.cpp

---
+++ Question1.cpp
@@ -0,0 +1,11 @@
+//centimeter into meter and kilometer
+#include<iostream>
+using namespace std;
+int main(){
+float a;
+cout<< "enter centimeter = ";
+cin>>a;
+cout<< a/100000 <<" kilometer\n";
+cout<< a/100 <<" meter ";
+return 0;
+}
