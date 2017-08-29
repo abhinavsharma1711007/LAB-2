7b3729f70945cffe85cec17a4534e846a1666632
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:47:51 +0530

    Create Question8.cpp

---
+++ Question8.cpp
@@ -0,0 +1,12 @@
+//Area Of Triangle
+#include<iostream>
+using namespace std;
+int main(){
+float b,p;
+cout<<"Enter Base=\t ";
+cin>>b;
+cout<<"Enter Perpendicular=\t ";
+cin>>p;
+cout<<"Area of triangle=\t "<<0.5*b*p;
+return 0;
+}
