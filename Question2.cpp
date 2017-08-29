a94437189a49e029c2ecf68027932e734ae31b59
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:43:43 +0530

    Create Question2.cpp

---
+++ Question2.cpp
@@ -0,0 +1,10 @@
+//celsius into Fahrenheit
+#include<iostream>
+using namespace std;
+int main(){
+float a;
+cout << "enter celsius = ";
+cin>> a;
+cout <<"Fahrenheit = " << a*1.8 + 32 ;
+return 0;
+}
