9e9658aa023740233bc07e65ef25178c722337c8
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:52:29 +0530

    Create Question11.cpp

---
+++ Question11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+float a,b,c;
+cout<< "Priciple=(rupee)\t ";
+cin>>a;
+cout<< "Rate=(%)\t ";
+cin>>b;
+cout<< "Time=(year)\t ";
+cin>>c;
+cout<< "SI= "<<a*b*c*0.01;
+return 0;
+}
