6b1f1ebfb07364e10976da20d1646dfa49deb448
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:46:00 +0530

    Create Question5.cpp

---
+++ Question5.cpp
@@ -0,0 +1,13 @@
+//x^y
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+float x,y;
+cout<< "Enter x\t";
+cin>> x;
+cout<< "Enter y\t";
+cin>>y;
+cout<< "x^y=\t"<< pow(x,y);
+return 0;
+}
