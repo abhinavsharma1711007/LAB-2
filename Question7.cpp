961aee4ec3dafb328ef8a113f5c2b8fd9782f47e
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:47:16 +0530

    Create Question7.cpp

---
+++ Question7.cpp
@@ -0,0 +1,13 @@
+//Third Angle
+#include<iostream>
+using namespace std;
+int main(){
+float a, b;
+cout<<"Enter I angle(in degree)\t ";
+cin>> a;
+cout<<"Enter II angle(in degree)\t ";
+cin>> b;
+cout<< "Third angle=\t" << 180 - a - b;
+return 0;
+}
+
