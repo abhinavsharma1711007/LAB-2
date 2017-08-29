021debeb4d302841973f469279bbfc282558c10a
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:48:37 +0530

    Create Question9.cpp

---
+++ Question9.cpp
@@ -0,0 +1,12 @@
+//Area Of Eq. Triangle
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+float a;
+cout<<"Enter Side Length=\t ";
+cin>> a;
+cout<<"Area=\t "<<a*a*sqrt(3)*0.25;
+return 0;
+}
+
