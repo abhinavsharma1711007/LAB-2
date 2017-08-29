135b5a5f162a764e3213b50e921c55d83ad367e1
abhinavsharma1711007 <31301667+abhinavsharma1711007@users.noreply.github.com>
2017-08-29 16:46:38 +0530

    Create Question6.cpp

---
+++ Question6.cpp
@@ -0,0 +1,11 @@
+//SQUARE ROOT
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+float x;
+cout<< "Enter number =\t";
+cin>>x;
+cout<< "square root = " <<pow(x,0.5);
+return 0;
+}
